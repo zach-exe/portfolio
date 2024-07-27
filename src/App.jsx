e42f7cc6f47ba6682be054a777b86182ae27979d
zach-exe <80307629+zach-exe@users.noreply.github.com>
2024-07-27 10:26:28 -0500

    fixed banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,21 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
+//import { ReactComponent as BannerSvg } from './assets/website-header.svg?component'
+import banner_img from './assets/website-header.svg'
 
-function App() {
+const App = () => {
   //const [count, setCount] = useState(0)
 
   return (
-    <div style={{width: '100%', height: '100%', paddingBottom: 694, background: '#302828', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 14, display: 'inline-flex'}}>
-<div style={{width: 1512, height: 209, position: 'relative'}}>
-<div style={{width: 709, height: 135, left: 402, top: 37, position: 'absolute', color: 'white', fontSize: 96, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Zach Shumway<br/></div>
-<div style={{width: 1512, height: 209, left: 1512, top: 209, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', background: 'linear-gradient(0deg, rgba(115, 115, 115, 0.80) 0%, rgba(217, 217, 217, 0) 100%)', border: '1px rgba(0, 0, 0, 0) solid'}} />
-</div>
-<div style={{width: 1139, height: 65, position: 'relative'}}>
-<div style={{width: 200, height: 63, left: 939, top: 2, position: 'absolute', color: 'white', fontSize: 48, fontFamily: 'Istok Web', fontWeight: '400', wordWrap: 'break-word'}}>About</div>
-<div style={{width: 218, height: 63, left: 0, top: 1, position: 'absolute', color: 'white', fontSize: 48, fontFamily: 'Istok Web', fontWeight: '400', wordWrap: 'break-word'}}>Home</div>
-<div style={{width: 335, height: 63, left: 411, top: 0, position: 'absolute', color: 'white', fontSize: 48, fontFamily: 'Istok Web', fontWeight: '400', wordWrap: 'break-word'}}>Exerpience</div>
-</div>
-</div>
-  )
-}
+    <div className="app">
+      <header className='banner'>
+        <img src={banner_img} className="banner-image"></img>
+      </header>
+      <h1>Hello World</h1>
+    </div>
+    
+  );
+};
 
 export default App
